6c2e39c2f5d8bcbd9e3d7adef520ce35af760aa5
ayham-kattan <ayhamkattan1@gmail.com>
2021-12-12 11:38:39 +0200

    done

--- model/userModel.js
+++ model/userModel.js
@@ -1,10 +1,14 @@
-const mongoose = require('mongoose')
-let User = mongoose.model('User', new mongoose.Schema({
+var mongoose = require("mongoose");
+
+var imageSchema = new mongoose.Schema({
   name: String,
   desc: String,
   img: {
     data: Buffer,
-    contentType:String
-  }
-}))
-module.exports = User;
+    contentType: String,
+  },
+});
+
+//Image is a model which has a schema imageSchema
+
+module.exports = new mongoose.model("Image", imageSchema);
